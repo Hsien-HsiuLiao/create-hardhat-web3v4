4811ad908bcf7b6ce3dc470608ebd9b3534f7083
jdevcs <junaid@chainsafe.io>
2024-08-05 15:21:33 +0200

    hardhat conf

---
+++ hardhat.config.ts
@@ -0,0 +1,8 @@
+import type { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-web3-v4";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+};
+
+export default config;
