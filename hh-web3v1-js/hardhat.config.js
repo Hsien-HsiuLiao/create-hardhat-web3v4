3ae95395e521fc04b2193075a81b85a89524d922
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2024-12-09 13:25:37 -0800

    create js template web3v1

---
+++ hh-web3v1-js/hardhat.config.js
@@ -0,0 +1,9 @@
+require("@nomicfoundation/hardhat-toolbox");
+//require("@nomicfoundation/hardhat-web3-v4");
+require("@nomiclabs/hardhat-web3");
+
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.24",
+};
